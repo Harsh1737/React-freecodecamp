467b609f88e43ca3f6bffb80a2c269308d92b097
Harsh <harshssnl88988@gmail.com>
2024-05-19 11:19:20 +0530

    commit-1

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
 
 function App() {
+
+  const [cnt, setcnt] = useState(10);
+
+  const add = () =>{
+    setcnt((pre)=>pre+1);
+    setcnt((pre)=>pre+1 );
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className="bg-green-500">hello {cnt}</h1>
+      <button onClick={add}>Add</button>
+    </>
   );
 }
-
 export default App;
